518a2f2649df09367a3e9b5ada535980702c4f5b
Ningyuan Pan <pan.neo@hotmail.com>
2020-03-28 22:32:38 +0800

    Modify logging aspect for RS services

--- src/main/aspect/ningyuan/pan/servicex/aspect/logging/TraceAspect.java
+++ src/main/aspect/ningyuan/pan/servicex/aspect/logging/TraceAspect.java
@@ -22,10 +22,14 @@ public class TraceAspect {
 	
 	/*
 	// Log method entering information
-	@Pointcut("execution(* ningyuan.pan.servicex.impl.*.*(..))")
+	@Pointcut("execution(* ningyuan.pan.servicex.impl.*.*(..))"
+			+ " || "
+			+ "execution(* ningyuan.pan.servicex.webservice.rs.impl.*.*(..))")
 	private void exeAllMethods() {};
 	
-	@Pointcut("!within(ningyuan.pan.servicex.impl.Test*)")
+	@Pointcut("!within(ningyuan.pan.servicex.impl.Test*)"
+			+ " && "
+			+ "!within(ningyuan.pan.servicex.webservice.rs.impl.Test*)")
 	private void notInJunitClassese() {};
 	
 	
@@ -37,16 +41,36 @@ public class TraceAspect {
 		LOGGER.trace(object.getClass().getName()+"."+s.getName()+"()");
 	}
 	
+	// Log aspect entering information (not woven in advice methods)
+	@Pointcut("execution(* ningyuan.pan.servicex.aspect.transaction.*.*(..))")
+	private void exeAllTAspectMethods() {};
+	
+	@Pointcut("within(ningyuan.pan.servicex.aspect.transaction.*)")
+	private void inTAspectClass() {};
+	
+	@Pointcut("!within(ningyuan.pan.servicex.aspect.Test*)")
+	private void notInAspectJunitClass() {};
+	
+	@Before("exeAllTAspectMethods()")  
+	public void logAspectMethod(JoinPoint joinPoint) throws Throwable {
+		Object object = joinPoint.getThis();
+		Signature s = joinPoint.getSignature();
+		
+		LOGGER.trace(object.getClass().getName()+"."+s.getName()+"()");
+	}
+	
 	// Log exception information
 	@Pointcut("handler(Throwable+) && args(e)")
 	private void exceptionHandler(Throwable e) {};
 	
-	@Pointcut("withincode(* ningyuan.pan.servicex.impl.*.*(..))")
+	@Pointcut("withincode(* ningyuan.pan.servicex.impl.*.*(..))"
+			+ " || "
+			+ "withincode(* ningyuan.pan.servicex.webservice.rs.impl.*.*(..))")
 	private void inAllMethods() {}
 	
 	@Before("exceptionHandler(e) && inAllMethods() && notInJunitClassese()")
 	public void logException(JoinPoint joinPoint, Throwable e) {
 		LOGGER.trace(ExceptionUtils.printStackTraceToString(e));
-	}
-	*/
+	}*/
+	
 }
