2c2cc4e151b5b54b11fe2d7dcfc49b226b82c640
Ningyuan Pan <pan.neo@hotmail.com>
2020-04-01 00:06:40 +0800

    Integrate JMS with ActiveMQ Artemis

--- src/main/java/ningyuan/pan/servicex/impl/XServiceMybatisImpl.java
+++ src/main/java/ningyuan/pan/servicex/impl/XServiceMybatisImpl.java
@@ -3,6 +3,13 @@
  */
 package ningyuan.pan.servicex.impl;
 
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 import org.apache.ibatis.session.SqlSession;
 
 import ningyuan.pan.servicex.XService;
@@ -26,20 +33,54 @@ public class XServiceMybatisImpl implements XService {
 		@SuppressWarnings("unchecked")
 		DataSourceManager<SqlSession> dataSourceManager = (DataSourceManager<SqlSession>)ServiceXUtil.getInstance().getGelobalObject(GlobalObjectName.MYBATIS_DATA_SOURCE_MANAGER);
 		
+		String msg = null;
 		if(dataSourceManager != null) {
 			try {
 				// transaction aspect will weave the code which initiates the thread local connection
 				// so we can use get instead of initAndGet
 				UserDAO userDAO = dataSourceManager.getThreadLocalConnection().getMapper(UserDAO.class);
 				User user = userDAO.findUserByID(0);
-				return user.getFirstName();
+				msg = user.getFirstName();
 			}
 			catch(Exception e) {
-				return "No user";
+				msg = "No user";
 			}
 		}
 		else {
-			return "No data source manager set in context";
+			msg = "No data source manager set in context";
+		}
+		
+		sendJMSMessage(msg);
+		
+		return msg;
+	}
+	
+	private void sendJMSMessage(String msg) {
+		@SuppressWarnings("unchecked")
+		DataSourceManager<Session> dataSourceManager = (DataSourceManager<Session>)ServiceXUtil.getInstance().getGelobalObject(GlobalObjectName.JMS_DATA_SOURCE_MANAGER);
+		
+		if(dataSourceManager != null) {
+			Session session = dataSourceManager.initAndGetThreadLocalConnection();
+			
+			try {
+				Destination queue = session.createQueue("anycast");
+				
+				MessageProducer producer = session.createProducer(queue);
+				
+				producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+				
+				TextMessage message = session.createTextMessage(msg);
+				
+				producer.send(message);
+				
+				producer.close();
+			}
+			catch (JMSException e) {
+				
+			}
+			finally {
+				dataSourceManager.removeAndCloseThreadLocalConnection();
+			}
 		}
 	}
 }
