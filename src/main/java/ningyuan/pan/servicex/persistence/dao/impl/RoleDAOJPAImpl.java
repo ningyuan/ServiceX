51340d073150ceb072282022949686b5354e9f6e
Ningyuan Pan <pan.neo@hotmail.com>
2020-04-25 23:25:36 +0800

    Add JPA RoleDAO

---
+++ src/main/java/ningyuan/pan/servicex/persistence/dao/impl/RoleDAOJPAImpl.java
@@ -0,0 +1,147 @@
+/**
+ * 
+ */
+package ningyuan.pan.servicex.persistence.dao.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TransactionRequiredException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ningyuan.pan.servicex.persistence.dao.RoleDAO;
+import ningyuan.pan.servicex.persistence.entity.Role;
+import ningyuan.pan.util.exception.ExceptionUtils;
+import ningyuan.pan.util.persistence.DataSourceManager;
+
+/**
+ * @author ningyuan
+ *
+ */
+public class RoleDAOJPAImpl implements RoleDAO {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RoleDAOJPAImpl.class);
+	
+	private final DataSourceManager<EntityManager> dataSourceManager;
+	
+	private String selectAllRole = "SELECT r FROM Role r";
+	
+	public RoleDAOJPAImpl(DataSourceManager<EntityManager> dataSourceManager) {
+		this.dataSourceManager = dataSourceManager;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Role> findAllRole() {
+		LOGGER.debug("findAllRole()");
+		
+		List<Role> ret = new ArrayList<Role>();
+		EntityManager con = null;
+		
+		con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				Query query = con.createQuery(selectAllRole);
+				ret = query.getResultList();
+			}
+		}
+		catch(Exception e) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+		}
+			
+		return ret;
+	}
+
+	@Override
+	public Role findRoleByID(byte id) {
+		LOGGER.debug("findRoleByID()");
+		
+		Role ret = null;
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				ret = con.find(Role.class, id);
+			}
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(iae));
+		}
+		
+		return ret;
+	}
+
+	@Override
+	public boolean add(Role role) {
+		LOGGER.debug("add()");
+		
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				con.persist(role);
+				
+				return true;
+			}
+		}
+		catch (EntityExistsException | IllegalArgumentException | TransactionRequiredException e) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean delete(byte id) {
+		LOGGER.debug("delete()");
+		
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				Role role = con.find(Role.class, id);
+				
+				if(role != null) {
+					con.remove(role);
+					
+					return true;
+				}
+			}
+		}
+		catch (Exception e) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean update(Role role) {
+		LOGGER.debug("update()");
+		
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				Role oldRole = con.find(Role.class, role.getID());
+				
+				if(oldRole != null) {
+					con.merge(role);
+					
+					return true;
+				}
+			}
+		}
+		catch (IllegalArgumentException | TransactionRequiredException e) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+		}
+		
+		return false;
+	}
+
+}
---
+++ src/test/java/ningyuan/pan/servicex/persistence/dao/impl/TestRoleDAOJPAImpl.java
@@ -0,0 +1,117 @@
+/**
+ * 
+ */
+package ningyuan.pan.servicex.persistence.dao.impl;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import ningyuan.pan.servicex.persistence.dao.RoleDAO;
+import ningyuan.pan.servicex.persistence.entity.Role;
+import ningyuan.pan.servicex.persistence.entity.User;
+import ningyuan.pan.util.persistence.DataSourceManager;
+import ningyuan.pan.util.persistence.JPADataSourceManager;
+
+/**
+ * @author ningyuan
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestRoleDAOJPAImpl {
+	
+	private static RoleDAO DAO;
+	
+	private static DataSourceManager<EntityManager> DATA_SOURCE_MANAGER;
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		/*DATA_SOURCE_MANAGER = new JPADataSourceManager();
+		
+		DAO = new RoleDAOJPAImpl(DATA_SOURCE_MANAGER);*/
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		//DATA_SOURCE_MANAGER.close();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		//DATA_SOURCE_MANAGER.initAndGetThreadLocalConnection().getTransaction().begin();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		//DATA_SOURCE_MANAGER.getThreadLocalConnection().getTransaction().commit();
+	}
+
+	@Test
+	public void test01GetRole() {
+		/*Role role = DAO.findRoleByID((byte)2);
+		System.out.println("!!!"+role);*/
+	}
+	
+	@Test
+	public void test02GetAllRole() {
+		/*List<Role> roles = DAO.findAllRole();
+		for(Role role : roles) {
+			System.out.println("!!!"+role);
+		}*/
+	}
+	
+	@Test
+	public void test03AddDup() {
+		/*Role role = new Role();
+		role.setID((byte) 0);
+		role.setName("non-root");
+		
+		assertFalse(DAO.add(role));*/
+	}
+	
+	@Test
+	public void test04Add() {
+		/*Role role = new Role();
+		role.setID((byte) 3);
+		role.setName("test");
+		
+		assertTrue(DAO.add(role));*/
+	}
+	
+	@Test
+	public void test05Update() {
+		/*Role role = new Role();
+		role.setID((byte) 3);
+		role.setName("test1");
+		
+		assertTrue(DAO.update(role));*/
+	}
+	
+	@Test
+	public void test06Delete() {
+		//assertTrue(DAO.delete((byte)3));
+	}
+}
