fdbb19238fbc956e2da7b703b7f7bf16669f6125
Ningyuan Pan <pan.neo@hotmail.com>
2020-04-26 16:11:59 +0800

    Add JPA UserDAO

---
+++ src/main/java/ningyuan/pan/servicex/persistence/dao/impl/UserDAOJPAImpl.java
@@ -0,0 +1,146 @@
+/**
+ * 
+ */
+package ningyuan.pan.servicex.persistence.dao.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TransactionRequiredException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ningyuan.pan.servicex.persistence.dao.UserDAO;
+import ningyuan.pan.servicex.persistence.entity.User;
+import ningyuan.pan.util.exception.ExceptionUtils;
+import ningyuan.pan.util.persistence.DataSourceManager;
+
+/**
+ * @author ningyuan
+ *
+ */
+public class UserDAOJPAImpl implements UserDAO {
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserDAOJPAImpl.class);
+	
+	private final DataSourceManager<EntityManager> dataSourceManager;
+	
+	private String selectAllUser = "SELECT u FROM User u";
+	
+	public UserDAOJPAImpl(DataSourceManager<EntityManager> dataSourceManager) {
+		this.dataSourceManager = dataSourceManager;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> findAllUser() {
+		LOGGER.debug("findAllUser()");
+		
+		List<User> ret = new ArrayList<User>();
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				Query query = con.createQuery(selectAllUser);
+				ret = query.getResultList();
+			}
+		}
+		catch(Exception e) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+		}
+		
+		return ret;
+	}
+
+	@Override
+	public User findUserByID(long id) {
+		LOGGER.debug("findUserByID()");
+		
+		User ret = null;
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				ret = con.find(User.class, id);
+			}
+		}
+		catch(IllegalArgumentException iae) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(iae));
+		}
+		
+		return ret;
+	}
+
+	@Override
+	public boolean add(User user) {
+		LOGGER.debug("add()");
+		
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				con.persist(user);
+				
+				return true;
+			}
+		}
+		catch (EntityExistsException | IllegalArgumentException | TransactionRequiredException e) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean delete(long id) {
+		LOGGER.debug("delete()");
+		
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				User user = con.find(User.class, id);
+				
+				if(user != null) {
+					con.remove(user);
+					
+					return true;
+				}
+			}
+		}
+		catch (IllegalArgumentException | TransactionRequiredException e) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean update(User user) {
+		LOGGER.debug("update()");
+		
+		EntityManager con = dataSourceManager.initAndGetThreadLocalConnection();
+		
+		try {
+			if(con != null) {
+				User oldUser = con.find(User.class, user.getID());
+				
+				if(oldUser != null) {
+					con.merge(user);
+					
+					return true;
+				}
+			}
+		}
+		catch (IllegalArgumentException | TransactionRequiredException e) {
+			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+		}
+		
+		return false;
+	}
+
+}
---
+++ src/test/java/ningyuan/pan/servicex/persistence/dao/impl/TestUserDAOJPAImpl.java
@@ -0,0 +1,221 @@
+/**
+ * 
+ */
+package ningyuan.pan.servicex.persistence.dao.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.RollbackException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import ningyuan.pan.servicex.persistence.dao.UserDAO;
+import ningyuan.pan.servicex.persistence.entity.Role;
+import ningyuan.pan.servicex.persistence.entity.User;
+import ningyuan.pan.util.persistence.DataSourceManager;
+import ningyuan.pan.util.persistence.JPADataSourceManager;
+
+/**
+ * @author ningyuan
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestUserDAOJPAImpl {
+	private static UserDAO DAO;
+	
+	private static DataSourceManager<EntityManager> DATA_SOURCE_MANAGER;
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		/*DATA_SOURCE_MANAGER = new JPADataSourceManager();
+		
+		DAO = new UserDAOJPAImpl(DATA_SOURCE_MANAGER);*/
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		//DATA_SOURCE_MANAGER.close();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		//DATA_SOURCE_MANAGER.initAndGetThreadLocalConnection().getTransaction().begin();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		/*if(DATA_SOURCE_MANAGER.getThreadLocalConnection().getTransaction().isActive()) {
+			try {
+				DATA_SOURCE_MANAGER.getThreadLocalConnection().getTransaction().commit();
+			}
+			catch(RollbackException re) {}
+		}*/
+	}
+
+	@Test
+	public void test01GetUser() {
+		/*User user = DAO.findUserByID(1L);
+		
+		System.out.println("!!!"+user);*/
+	}
+	
+	@Test
+	public void test02GetAllUser() {
+		/*List<User> users = DAO.findAllUser();
+		for(User user : users) {
+			System.out.println("!!!"+user);
+		}*/
+	}
+	
+	@Test
+	public void test03AddDup() {
+		/*User user = new User();
+		user.setID(0L);
+		user.setFirstName("test");
+		user.setLastName("test");
+		
+		assertFalse(DAO.add(user));*/
+	}
+	
+	/*@Test(expected = AssertionError.class)
+	public void test04AddNoCascade() {
+		User user = new User();
+		user.setID(3L);
+		user.setFirstName("test");
+		user.setLastName("test");
+		
+		// a role with id 3 dose not exist
+		// the user will be not added
+		// no cascade persist
+		Role role = new Role();
+		role.setID((byte)3);
+		role.setName("test");
+		
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(role);
+		
+		user.setRoles(roles);
+		
+		assertFalse(DAO.add(user));
+	}*/
+	
+	@Test
+	public void test05AddNoCascade() {
+		/*User user = new User();
+		user.setID(3L);
+		user.setFirstName("test");
+		user.setLastName("test");
+		
+		Role role = new Role();
+		// a role with id 1 exists
+		role.setID((byte)1);
+		// the name will be not updated
+		role.setName("test");
+		
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(role);
+		
+		user.setRoles(roles);
+		
+		assertTrue(DAO.add(user));*/
+	}
+	
+	@Test
+	public void test06Add() {
+		/*User user = new User();
+		user.setID(4L);
+		user.setFirstName("test");
+		user.setLastName("test");
+		
+		List<Role> roles = new ArrayList<Role>();
+		
+		Role role = new Role();
+		// a role with id 1 exists
+		role.setID((byte)1);
+		
+		roles.add(role);
+		
+		role = new Role();
+		// a role with id 1 exists
+		role.setID((byte)2);
+		
+		roles.add(role);
+		
+		user.setRoles(roles);
+		
+		assertTrue(DAO.add(user));*/
+	}
+	
+	/*@Test(expected = EntityNotFoundException.class)
+	public void test07UpdateNoCascade() {
+		User user = new User();
+		user.setID(3L);
+		user.setFirstName("test1");
+		user.setLastName("test1");
+		
+		Role role = new Role();
+		// a role with id 3 dose not exist
+		// the user will be not updated
+		// no cascade merge
+		role.setID((byte)3);
+		role.setName("test1");
+		
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(role);
+		
+		user.setRoles(roles);
+		
+		DAO.update(user);
+	}*/
+	
+	@Test
+	public void test08UpdateNoCascade() {
+		/*User user = new User();
+		user.setID(3L);
+		user.setFirstName("test1");
+		user.setLastName("test1");
+		
+		Role role = new Role();
+		// a role with id 1 exists
+		role.setID((byte)1);
+		// the name will not be updated
+		// no cascade merge
+		role.setName("test1");
+		
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(role);
+		
+		user.setRoles(roles);
+		
+		assertTrue(DAO.update(user));*/
+	}
+	
+	@Test
+	public void test09Delete() {
+		//assertTrue(DAO.delete(4L));
+	} 
+}
